92c409ef3ebdd896adebd00df732cdebd9c0f51a
Frozen Burnside <fmilesi_soft@hotmail.com>
2026-01-15 00:02:11 +0000

    Define screen split (Contributor: @Mc-Muffin)

---
+++ src/screen.c
@@ -0,0 +1,45 @@
+INLINE_ASM(_func_8C0F1D90, 0x66, "asm/nonmatching/./screen/_func_8C0F1D90.src");
+
+// MERGE_LIST([["h'0002A5B8", '_lbl_8C0F1F80'], ["h'0002A5C8", '_lbl_8C0F1F84'], ['_sys', '_lbl_8C0F1F7C'], ["h'0002A5C4", '_lbl_8C0F1F88']]);
+INLINE_ASM(_bhSetScreenFade, 0x164, "asm/nonmatching/./screen/_bhSetScreenFade.src");
+
+// MERGE_LIST([["h'0002A5B8", '_lbl_8C0F1F80'], ["h'0002A5BC", '_lbl_8C0F1FA0'], ["h'0002A5C8", '_lbl_8C0F1F84'], ["h'4F800000", '_lbl_8C0F1FA4'], ["h'0002A5C4", '_lbl_8C0F1F88'], ["h'0002A5F0", '_lbl_8C0F1F9C'], ["h'4F000000", '_lbl_8C0F1F8C'], ["h'0002A5D4", '_lbl_8C0F1F90'], ["h'0002A5E0", '_lbl_8C0F1F94'], ["h'0002A5EC", '_lbl_8C0F1F98'], ['_sys', '_lbl_8C0F1F7C']]);
+INLINE_ASM(_bhControlScreenFade, 0x122, "asm/nonmatching/./screen/_bhControlScreenFade.src");
+
+// MERGE_LIST([["h'0002A5D4", '_lbl_8C0F2200'], ["h'0002A5D8", '_lbl_8C0F2204'], ["h'0002A5E0", '_lbl_8C0F2208'], ["h'0002A5E4", '_lbl_8C0F220C'], ["h'0002A5BC", '_lbl_8C0F21F0'], ["h'0002A5EC", '_lbl_8C0F2210'], ["h'0002A5C0", '_lbl_8C0F21F4'], ["h'0002A5CC", '_lbl_8C0F21FC'], ["h'0002A5C8", '_lbl_8C0F21F8']]);
+INLINE_ASM(_bhDrawScreenFade, 0xfe, "asm/nonmatching/./screen/_bhDrawScreenFade.src");
+
+// MERGE_LIST([["h'4F000000", '_lbl_8C0F2220'], ["h'0002A5D0", '_lbl_8C0F2224'], ["h'0002A5DC", '_lbl_8C0F2228'], ["h'BF4F5C29", '_lbl_8C0F2238'], ["h'0002A5E8", '_lbl_8C0F222C'], ["h'44200000", '_lbl_8C0F2230'], ['_lbl_8C17B714', '_lbl_8C0F2214'], ["h'43F00000", '_lbl_8C0F2234'], ['_sys', '_lbl_8C0F2218'], ["h'0002A5C4", '_lbl_8C0F221C'], ['_lbl_8C199168', '_lbl_8C0F223C']]);
+INLINE_ASM(_func_8C0F217A, 0x2d0, "asm/nonmatching/./screen/_func_8C0F217A.src");
+
+// MERGE_LIST([["h'44200000", '_lbl_8C0F2650'], ["h'BF63D70A", '_lbl_8C0F2654']]);
+INLINE_ASM(_bhInitScreenSaver, 0x32, "asm/nonmatching/./screen/_bhInitScreenSaver.src");
+
+// MERGE_LIST([['_sys', '_lbl_8C0F2658'], ["h'0002A5F4", '_lbl_8C0F265C']]);
+INLINE_ASM(_bhSetScreenSaver, 0x44, "asm/nonmatching/./screen/_bhSetScreenSaver.src");
+
+// MERGE_LIST([['_sys', '_lbl_8C0F2658'], ["h'0002A60C", '_lbl_8C0F2664'], ["h'0002A5F4", '_lbl_8C0F265C'], ["h'4F800000", '_lbl_8C0F2660']]);
+INLINE_ASM(_bhControlScreenSaver, 0x5c, "asm/nonmatching/./screen/_bhControlScreenSaver.src");
+
+// MERGE_LIST([["h'0002A60C", '_lbl_8C0F2664'], ["h'0002A608", '_lbl_8C0F2668'], ["h'0002A600", '_lbl_8C0F266C'], ['_sys', '_lbl_8C0F2658'], ["h'0002A5F4", '_lbl_8C0F265C']]);
+INLINE_ASM(_bhDrawScreenSaver, 0xcc, "asm/nonmatching/./screen/_bhDrawScreenSaver.src");
+
+// MERGE_LIST([["h'4F000000", '_lbl_8C0F2680'], ["h'3FA00000", '_lbl_8C0F2684'], ["h'0002A608", '_lbl_8C0F2668'], ["h'43F00000", '_lbl_8C0F2688'], ['_sys', '_lbl_8C0F2658'], ['_njDrawPolygon', '_lbl_8C0F268C'], ['_lbl_8C17B714', '_lbl_8C0F2670'], ["h'44200000", '_lbl_8C0F2650'], ["h'41A00000", '_lbl_8C0F2674'], ["h'41700000", '_lbl_8C0F2678'], ['_lbl_8C17B8B6', '_lbl_8C0F267C']]);
+INLINE_ASM(_bhControlCinesco, 0xb8, "asm/nonmatching/./screen/_bhControlCinesco.src");
+
+INLINE_ASM(_bhDrawCinesco, 0x124, "asm/nonmatching/./screen/_bhDrawCinesco.src");
+
+INLINE_ASM(_bhDrawScope, 0x52c, "asm/nonmatching/./screen/_bhDrawScope.src");
+
+INLINE_ASM(_func_8C0F2CF0, 0xae, "asm/nonmatching/./screen/_func_8C0F2CF0.src");
+
+// MERGE_LIST([['_lbl_8C167CDC', '_lbl_8C0F2DA0'], ["h'41800000", '_lbl_8C0F2DA4'], ["h'80000060", '_lbl_8C0F2DA8'], ["h'BFB33333", '_lbl_8C0F2DAC'], ['_lbl_8C199168', '_lbl_8C0F2DB0']]);
+INLINE_ASM(_func_8C0F2D9E, 0x3d8, "asm/nonmatching/./screen/_func_8C0F2D9E.src");
+
+// MERGE_LIST([["h'0000008C", 140], ["h'0002A6DD", '_lbl_8C0F322C'], ["h'43900000", '_lbl_8C0F3230'], ["h'0002A6DC", '_lbl_8C0F3234'], ["h'0002A6DE", '_lbl_8C0F3238'], ["h'43A80000", '_lbl_8C0F323C'], ["h'0002A6DF", '_lbl_8C0F3240']]);
+INLINE_ASM(_func_8C0F3176, 0xe6, "asm/nonmatching/./screen/_func_8C0F3176.src");
+
+INLINE_ASM(_bhDrawFullScreenRenderTexture, 0x454, "asm/nonmatching/./screen/_bhDrawFullScreenRenderTexture.src");
+
+INLINE_ASM(_bhDrawSmallScreenRenderTexture, 0x310, "asm/nonmatching/./screen/_bhDrawSmallScreenRenderTexture.src");
+
---
+++ src/split1.c
@@ -0,0 +1,74 @@
+INLINE_ASM(_bhInitCamera, 0x74, "asm/nonmatching/./split1/_bhInitCamera.src");
+
+INLINE_ASM(_bhControlCamera, 0x17c, "asm/nonmatching/./split1/_bhControlCamera.src");
+
+INLINE_ASM(_bhMakeCameraVector, 0x5a, "asm/nonmatching/./split1/_bhMakeCameraVector.src");
+
+// MERGE_LIST([['_lbl_8C13A510', '_lbl_8C0F3C80'], ['__quick_odd_mvn', '_lbl_8C0F3C84'], ['_njUnitMatrix', '_lbl_8C0F3C88'], ['_cam', '_lbl_8C0F3C8C'], ['_njRotateY', '_lbl_8C0F3C90'], ['_njRotateX', '_lbl_8C0F3C94'], ['_njRotateZ', '_lbl_8C0F3C98'], ['_njCalcVector', '_lbl_8C0F3C9C']]);
+INLINE_ASM(_bhGetCameraPosition, 0x9e, "asm/nonmatching/./split1/_bhGetCameraPosition.src");
+
+INLINE_ASM(_bhCheckCut, 0x384, "asm/nonmatching/./split1/_bhCheckCut.src");
+
+INLINE_ASM(_func_8C0F402C, 0xb4, "asm/nonmatching/./split1/_func_8C0F402C.src");
+
+INLINE_ASM(_func_8C0F40E0, 0xd2, "asm/nonmatching/./split1/_func_8C0F40E0.src");
+
+INLINE_ASM(_func_8C0F41B2, 0x86, "asm/nonmatching/./split1/_func_8C0F41B2.src");
+
+// MERGE_LIST([["h'00980080", 128], ["h'008002A8", 680], ["h'00C40098", 152]]);
+INLINE_ASM(_func_8C0F4238, 0x300, "asm/nonmatching/./split1/_func_8C0F4238.src");
+
+INLINE_ASM(_bhSetFixedCut, 0x1c0, "asm/nonmatching/./split1/_bhSetFixedCut.src");
+
+// MERGE_LIST([["h'04000200", 512], ["h'02A80400", 1024]]);
+INLINE_ASM(_func_8C0F46F8, 0x7e, "asm/nonmatching/./split1/_func_8C0F46F8.src");
+
+// MERGE_LIST([['_lbl_8C1411F8', '_lbl_8C0F4808'], ["h'008C02A8", 680], ['_cam', '_lbl_8C0F480C'], ["h'00C4008C", 140]]);
+INLINE_ASM(_func_8C0F4776, 0xde, "asm/nonmatching/./split1/_func_8C0F4776.src");
+
+// MERGE_LIST([["h'008400E0", 224], ["h'00E00098", 152], ["h'00A40084", 132]]);
+INLINE_ASM(_func_8C0F4854, 0x138, "asm/nonmatching/./split1/_func_8C0F4854.src");
+
+// MERGE_LIST([["h'01A401AC", 428], ["h'02A801A4", 420]]);
+INLINE_ASM(_func_8C0F498C, 0x95a, "asm/nonmatching/./split1/_func_8C0F498C.src");
+
+// MERGE_LIST([["h'00C000BC", 188], ["h'400000C0", 192]]);
+INLINE_ASM(_func_8C0F52E6, 0x182, "asm/nonmatching/./split1/_func_8C0F52E6.src");
+
+INLINE_ASM(_func_8C0F5468, 0xa3c, "asm/nonmatching/./split1/_func_8C0F5468.src");
+
+INLINE_ASM(_func_8C0F5EA4, 0x260, "asm/nonmatching/./split1/_func_8C0F5EA4.src");
+
+INLINE_ASM(_bhSetEventFixedCut, 0x1a4, "asm/nonmatching/./split1/_bhSetEventFixedCut.src");
+
+INLINE_ASM(_func_8C0F62A8, 0xbe, "asm/nonmatching/./split1/_func_8C0F62A8.src");
+
+// MERGE_LIST([["h'01A401AC", 428], ["h'00E001A4", 420], ["h'00B400E0", 224], ["h'00A400B4", 180], ['_lbl_8C1411F8', '_lbl_8C0F63D8'], ['_bhCkFlg', '_lbl_8C0F63DC']]);
+INLINE_ASM(_func_8C0F6366, 0x38c, "asm/nonmatching/./split1/_func_8C0F6366.src");
+
+// MERGE_LIST([["h'00000080", 128]]);
+INLINE_ASM(_func_8C0F66F2, 0x784, "asm/nonmatching/./split1/_func_8C0F66F2.src");
+
+// MERGE_LIST([["h'800000E0", 224], ["h'4F000000", '_lbl_8C0F6F8C'], ["h'CF800000", '_lbl_8C0F6F90']]);
+INLINE_ASM(_func_8C0F6E76, 0x3e4, "asm/nonmatching/./split1/_func_8C0F6E76.src");
+
+INLINE_ASM(_func_8C0F725A, 0x1a6, "asm/nonmatching/./split1/_func_8C0F725A.src");
+
+// MERGE_LIST([['_njCalcPoint', '_lbl_8C0F7480'], ["h'034002D8", 728], ["h'02A80340", 832]]);
+INLINE_ASM(_func_8C0F7400, 0x464, "asm/nonmatching/./split1/_func_8C0F7400.src");
+
+INLINE_ASM(_bhInitPlEyeCamera, 0x20a, "asm/nonmatching/./split1/_bhInitPlEyeCamera.src");
+
+// MERGE_LIST([["h'008400E0", 224], ["h'01080084", 132], ["h'02D80108", 264], ["h'0002A5A8", '_lbl_8C0F7B30'], ["h'0001B148", '_lbl_8C0F7B34']]);
+INLINE_ASM(_bhSetPlEyeCamera, 0x194, "asm/nonmatching/./split1/_bhSetPlEyeCamera.src");
+
+// MERGE_LIST([["h'04100800", 2048], ["h'02D80410", 1040], ["h'016802D8", 728], ["h'00B60168", 360], ["h'008800B6", 182], ["h'00840088", 136], ["h'00C40084", 132], ['_njCalcPoint', '_lbl_8C0F7C74']]);
+INLINE_ASM(_func_8C0F7C02, 0x4, "asm/nonmatching/./split1/_func_8C0F7C02.src");
+
+INLINE_ASM(_func_8C0F7C06, 0x76, "asm/nonmatching/./split1/_func_8C0F7C06.src");
+
+INLINE_ASM(_func_8C0F7C7C, 0x74, "asm/nonmatching/./split1/_func_8C0F7C7C.src");
+
+// MERGE_LIST([['_cam', '_lbl_8C0F7DA8'], ["h'000000C4", 196]]);
+INLINE_ASM(_func_8C0F7CF0, 0xc4, "asm/nonmatching/./split1/_func_8C0F7CF0.src");
+
