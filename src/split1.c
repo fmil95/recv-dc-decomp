b835be4a137c7c9d4a23e4afab00e08b02d91e65
Frozen Burnside <fmilesi_soft@hotmail.com>
2026-01-15 00:31:41 +0000

    Define sbinit split + remove unnecesary source file

--- src/split1.c
+++ src/split1.c
@@ -1,8 +1,3 @@
-INLINE_ASM(_sbInitSystem, 0x90, "asm/nonmatching/./split1/_sbInitSystem.src");
-
-// MERGE_LIST([['_njSetTextureMemorySize', '_lbl_8C010520'], ["h'00000100", 256], ['_lbl_8C178088', '_lbl_8C010538'], ['_syBtExit', '_lbl_8C010540'], ['_syHwInit', '_lbl_8C010504'], ['_njInitSystem', '_lbl_8C010524'], ["h'E0000000", '_lbl_8C010508'], ['_syHwInit2', '_lbl_8C010528'], ['_lbl_8C1F5EC8', '_lbl_8C01050C'], ['_gMapleSendBuf', '_lbl_8C01052C'], ["h'0D000000", '_lbl_8C010510'], ['_gMapleRecvBuf', '_lbl_8C010530'], ["h'80000000", '_lbl_8C010514'], ['_pdInitPeripheral', '_lbl_8C010534'], ['_InitGdSystemEx', '_lbl_8C01053C'], ['_syMallocInit', '_lbl_8C010518'], ["h'00140000", '_lbl_8C01051C'], ["h'0100FF0F", 65295]]);
-INLINE_ASM(_sbExitSystem, 0x94, "asm/nonmatching/./split1/_sbExitSystem.src");
-
 INLINE_ASM(_npPlusInit, 0x58, "asm/nonmatching/./split1/_npPlusInit.src");
 
 // MERGE_LIST([['_njpmemp', '_lbl_8C0106A0'], ['_lbl_8C320E34', '_lbl_8C0106A4'], ["h'00020000", '_lbl_8C0106A8'], ["h'00802218", 8728], ["h'0E140080", 128], ["h'00000E14", 3604]]);
