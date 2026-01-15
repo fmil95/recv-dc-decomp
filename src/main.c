9f2259ca4b1e51b5fa650f252298a74c100a5943
Frozen Burnside <fmilesi_soft@hotmail.com>
2026-01-15 00:13:08 +0000

    Define main split

---
+++ src/main.c
@@ -0,0 +1,12 @@
+INLINE_ASM(_njUserInit, 0x29a, "asm/nonmatching/./main/_njUserInit.src");
+
+// MERGE_LIST([['_BupInit', '_lbl_8C010400'], ['_InitSoundProgram', '_lbl_8C010404'], ['_bhInitVSync', '_lbl_8C010408'], ['_njSetBackColor', '_lbl_8C0103FC'], ['_lbl_8C12F480', '_lbl_8C01040C'], ['_Init_Expand', '_lbl_8C010410'], ['_tbuf', '_lbl_8C0103D4'], ['_njInitTexture', '_lbl_8C0103D8'], ['_njClipZ', '_lbl_8C0103F8'], ['_njSetPaletteMode', '_lbl_8C0103DC'], ['_njPrintSize', '_lbl_8C0103E0'], ['_BHD_ASPECT_Y', '_lbl_8C0103E4'], ['_BHD_ASPECT_X', '_lbl_8C0103E8'], ['_njSetAspect', '_lbl_8C0103EC'], ["h'C69C4000", '_lbl_8C0103F0'], ["h'C0000000", '_lbl_8C0103F4']]);
+INLINE_ASM(_njUserMain, 0x56, "asm/nonmatching/./main/_njUserMain.src");
+
+// MERGE_LIST([['_sys', '_lbl_8C010418'], ['_bhSysTaskJumpTab', '_lbl_8C010414'], ['_bhCheckSoftReset', '_lbl_8C01041C']]);
+INLINE_ASM(_njUserExit, 0x26, "asm/nonmatching/./main/_njUserExit.src");
+
+// MERGE_LIST([['_ExitSoundProgram', '_lbl_8C010420'], ['_BupExit', '_lbl_8C010424'], ['_njExitTexture', '_lbl_8C010428'], ['_njExitPrint', '_lbl_8C01042C'], ['_sbExitSystem', '_lbl_8C010430'], ['_syBtExit', '_lbl_8C010434']]);
+INLINE_ASM(_func_8C0103D2, 0x66, "asm/nonmatching/./main/_func_8C0103D2.src");
+
+// MERGE_LIST([["h'36C37601", 30209], ["h'0009420B", 16907], ["h'E04CFF27", 65319]]);
